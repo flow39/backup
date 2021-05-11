a59594aa0d926aaff9298820f3329e6ead8a600d
flo <flo@asgard.gipftlv.pro>
2021-05-11 23:00:31 +0200

    first commit

---
+++ backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#VARIABLES
+serv="asgard"
+mountCommand=""
+mountPoint="/srv/backup"
+cheminLog="/root/logs"
+dossierCible="/var/www/html"
+dossierCible1="/etc/apache2/sites-available"
+sendmail="sendmail"
+mailEnvoi="backup@machine.local"
+mailDestinataire="toto@toto.com"
+rsyncCommand="rsync -rlptD"
+
+#eval $mountCommand
+
+echo --- BACKUP $dossierCible ---
+echo début de la synchronisation @ $(date +%d-%m-%y_%H:%M)
+eval $rsyncCommand $dossierCible $mountPoint
+echo Synchronisation du dossier cible $dossierCible terminée à $(date +%H:%M)
+echo --- BACKUP $dossierCible1 ---
+echo début de la synchronisation @ $(date +%d-%m-%y_%H:%M)
+eval $rsyncCommand $dossierCible1 $mountPoint
+echo Synchronisation du dossier cible $dossierCible1 terminée à $(date +%H:%M)
+
+du -h $mountPoint
+
+#eval umount $mountPoint
+
+#RAPPORT
+echo Rapport envoyé @ $(date +%d-%m-%y_%H:%M)
+echo 'Subject:' "BACKUP $serv" | cat - "$cheminLog/backup-$serv.log" | $sendmail -r $mailEnvoi $mailDestinataire
+echo Rapport terminé
---
+++ exebackup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mountPoint="/srv/backup"
+mountCommand=""
+backupCommand="/root/scripts/./backup.sh &> /root/logs/backup-asgard.log"
+backupSqlCommand="/root/scripts/./backup_mysql.sh $> /root/logs/backup-asgard-mysql.log"
+
+eval $mountCommand
+eval $backupCommand
+eval $backupSqlCommand
+eval umount $mountPoint
