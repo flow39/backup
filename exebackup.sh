122b342dbc4bc329cbf556544ed0fc478ed6fa19
flow39 <flow39@flow39.comwq>
2021-05-24 15:31:50 +0200

    Exécution séquentielle des commandes dans le script exeBackup.sh

--- exebackup.sh
+++ exebackup.sh
@@ -4,7 +4,4 @@ mountCommand=""
 backupCommand="/root/scripts/./backup.sh &> /root/logs/backup-asgard.log"
 backupSqlCommand="/root/scripts/./backup_mysql.sh $> /root/logs/backup-asgard-mysql.log"
 
-eval $mountCommand
-eval $backupCommand
-eval $backupSqlCommand
-eval umount $mountPoint
+eval $mountCommand && eval $backupCommand && eval $backupSqlCommand && eval umount $mountPoint
