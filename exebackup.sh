a7925816d7ecde44301a73c7941b1fa5e0496ac0
flow39 <flow39@flow39.comwq>
2021-05-24 15:46:58 +0200

    Modification du chemin des scripts

--- exebackup.sh
+++ exebackup.sh
@@ -2,7 +2,7 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 mountPoint="/srv/backup"
 mountCommand=""
-backupCommand="/root/scripts/./backup.sh &> /root/logs/backup-asgard.log"
-backupSqlCommand="/root/scripts/./backup_mysql.sh $> /root/logs/backup-asgard-mysql.log"
+backupCommand="/root/backup/./backup.sh &> /root/logs/backup-asgard.log"
+backupSqlCommand="/root/backup/./backup_mysql.sh $> /root/logs/backup-asgard-mysql.log"
 
 eval $mountCommand && eval $backupCommand && eval $backupSqlCommand && eval umount $mountPoint
