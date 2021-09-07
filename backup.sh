1df6a114c6ea754930ac120e2c09ec45b3f22d65
flow39 <froidurot.florian@gmail.com>
2021-09-07 10:41:28 +0200

    ajout de l'option --delete-after à la commande RSYNC

--- backup.sh
+++ backup.sh
@@ -11,16 +11,17 @@ sendmail="sendmail"
 mailEnvoi="backup@machine.local"
 mailDestinataire="toto@toto.com"
 rsyncCommand="rsync -rlptD"
+rsyncOpt="--delete-after"
 
 #eval $mountCommand
 
 echo --- BACKUP $dossierCible ---
 echo début de la synchronisation @ $(date +%d-%m-%y_%H:%M)
-eval $rsyncCommand $dossierCible $mountPoint
+eval $rsyncCommand $dossierCible $mountPoint $rsyncOpt
 echo Synchronisation du dossier cible $dossierCible terminée à $(date +%H:%M)
 echo --- BACKUP $dossierCible1 ---
 echo début de la synchronisation @ $(date +%d-%m-%y_%H:%M)
-eval $rsyncCommand $dossierCible1 $mountPoint
+eval $rsyncCommand $dossierCible1 $mountPoint $rsyncOpt
 echo Synchronisation du dossier cible $dossierCible1 terminée à $(date +%H:%M)
 
 du -h $mountPoint
